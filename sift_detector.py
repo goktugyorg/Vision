13e26c4644d45eeb20f01defe69082e54b9cc31a
goktugyorg <goktugyorg@gmail.com>
2018-11-20 15:46:11 +0300

    sift

--- sift_detector.py
+++ sift_detector.py
@@ -1,14 +1,13 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("grid.png")
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-gray = np.float32(gray)
+img = cv2.imread('img.jpg')
+gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 sift = cv2.xfeatures2d.SIFT_create()
-
 kp = sift.detect(gray,None)
 
 img=cv2.drawKeypoints(gray,kp)
+
+cv2.imshow(img)
 cv2.imwrite('sift_keypoints.jpg',img)
