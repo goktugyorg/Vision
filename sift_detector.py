ca1b3957e300af60c4b846f316b42f9533bba5a0
goktugyorg <goktugyorg@gmail.com>
2018-11-22 15:52:27 +0300

    sift point detection working

--- sift_detector.py
+++ sift_detector.py
@@ -1,13 +1,16 @@
+
 import cv2
 import numpy as np
 
-img = cv2.imread('img.jpg')
+img = cv2.imread('grid.png')
 gray= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 sift = cv2.xfeatures2d.SIFT_create()
 kp = sift.detect(gray, None)
 
 img = cv2.drawKeypoints(gray, kp, img)
+sift.compute(gray,kp)
 
-# cv2.imshow(img)
+
 cv2.imwrite('sift_keypoints.jpg',img)
+
